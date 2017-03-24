419d29f2db551f8c9907bd79c353a2d1f56c058b
O365AES <andrescanello@outlook.com>
2017-03-24 16:30:12 +1000

    Add files via upload

---
+++ EnableMailboxAuditing.ps1
@@ -0,0 +1,13 @@
+﻿# This script enables Mailbox Audit Logging for a list of mailboxes.
+# Instructions:
+# Create "C:\temp\userlist.csv" with a list of all the email addresses of the users to be modified. Add the word "email" at the top.
+# Connect to Exchange Online Powershell before running -> http://aka.ms/EXOPSPreview https://technet.microsoft.com/en-us/library/mt775114%28v=exchg.160%29.aspx
+
+
+$users = Import-csv "C:\temp\userlist.csv"
+
+foreach ($user in $users)
+    {
+        Set-Mailbox -Identity $user.email -AuditLogAgeLimit 90 -AuditEnabled $true -AuditAdmin Update,Move,MoveToDeletedItems,SoftDelete,HardDelete,FolderBind,SendAs,SendOnBehalf,Create,Copy,MessageBind -AuditDelegate Update,Move,MoveToDeletedItems,SoftDelete,HardDelete,FolderBind,SendAs,SendOnBehalf,Create -AuditOwner Update,MoveToDeletedItems,Move,SoftDelete,HardDelete,Create,MailboxLogin
+
+    }
